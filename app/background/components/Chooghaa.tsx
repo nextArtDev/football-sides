ac3e93755dbd780e06c644b9f755e513dc83af3e
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-03-10 01:10:23 +0330

    complete choogha

---
+++ app/background/components/Chooghaa.tsx
@@ -0,0 +1,149 @@
+import React from 'react'
+
+type Props = {}
+
+function Chooghaa({}: Props) {
+  return (
+    <section className="bg-white flex">
+      <div className="grid grid-cols-3 grid-rows-6  h-[700px] w-[3rem]">
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+        <div className="col-span-1 row-span-5 bg-black"> </div>
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-6 h-[700px] w-[3rem]">
+        {/* Div6 */}
+        <div className="col-span-3 row-span-2 bg-black"> </div>
+
+        {/* Div7 */}
+        <div className="row-span-3 col-start-2 row-start-3 bg-black"> </div>
+      </div>
+      <div className="grid grid-cols-1 grid-rows-6  h-[700px] w-[1rem]">
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+      </div>
+      <div className="grid grid-cols-10 grid-rows-6 h-[700px] w-[12rem]">
+        <div className="col-span-9 bg-black  "> </div>
+
+        {/* Div 5 */}
+        <div className="row-span-4 row-start-2 bg-black  "> </div>
+
+        {/* Div 6 */}
+        <div className="col-span-5 col-start-3 row-start-2 bg-black  "></div>
+
+        {/* Div 7 */}
+        <div className="col-span-3 col-start-4 row-start-3 bg-black  "></div>
+
+        {/* Div 8 */}
+        <div className="row-span-2 row-start-4 col-start-5 bg-black  "></div>
+
+        {/* Div 9 */}
+        <div className="row-span-4 row-start-2 col-start-9 bg-black  "> </div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-6 h-[700px] w-[3rem]">
+        {/* Div6 */}
+        <div className="col-span-3 row-span-2 bg-black"> </div>
+
+        {/* Div7 */}
+        <div className="row-span-3 col-start-2 row-start-3 bg-black"> </div>
+      </div>
+      <div className="grid grid-cols-1 grid-rows-6  h-[700px] w-[1rem]">
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+      </div>
+      <div className="grid grid-cols-10 grid-rows-6 h-[700px] w-[12rem]">
+        <div className="col-span-9 bg-black  "> </div>
+
+        {/* Div 5 */}
+        <div className="row-span-4 row-start-2 bg-black  "> </div>
+
+        {/* Div 6 */}
+        <div className="col-span-5 col-start-3 row-start-2 bg-black  "></div>
+
+        {/* Div 7 */}
+        <div className="col-span-3 col-start-4 row-start-3 bg-black  "></div>
+
+        {/* Div 8 */}
+        <div className="row-span-2 row-start-4 col-start-5 bg-black  "></div>
+
+        {/* Div 9 */}
+        <div className="row-span-4 row-start-2 col-start-9 bg-black  "> </div>
+      </div>{' '}
+      <div className="grid grid-cols-10 grid-rows-6 h-[700px] w-[12rem]">
+        <div className="col-span-9 bg-black  "> </div>
+
+        {/* Div 5 */}
+        <div className="row-span-4 row-start-2 bg-black  "> </div>
+
+        {/* Div 6 */}
+        <div className="col-span-5 col-start-3 row-start-2 bg-black  "></div>
+
+        {/* Div 7 */}
+        <div className="col-span-3 col-start-4 row-start-3 bg-black  "></div>
+
+        {/* Div 8 */}
+        <div className="row-span-2 row-start-4 col-start-5 bg-black  "></div>
+
+        {/* Div 9 */}
+        <div className="row-span-4 row-start-2 col-start-9 bg-black  "> </div>
+      </div>{' '}
+      <div className="grid grid-cols-10 grid-rows-6 h-[700px] w-[12rem]">
+        <div className="col-span-9 bg-black  "> </div>
+
+        {/* Div 5 */}
+        <div className="row-span-4 row-start-2 bg-black  "> </div>
+
+        {/* Div 6 */}
+        <div className="col-span-5 col-start-3 row-start-2 bg-black  "></div>
+
+        {/* Div 7 */}
+        <div className="col-span-3 col-start-4 row-start-3 bg-black  "></div>
+
+        {/* Div 8 */}
+        <div className="row-span-2 row-start-4 col-start-5 bg-black  "></div>
+
+        {/* Div 9 */}
+        <div className="row-span-4 row-start-2 col-start-9 bg-black  "> </div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-6 h-[700px] w-[3rem]">
+        {/* Div6 */}
+        <div className="col-span-3 row-span-2 bg-black"> </div>
+
+        {/* Div7 */}
+        <div className="row-span-3 col-start-2 row-start-3 bg-black"> </div>
+      </div>
+      <div className="grid grid-cols-1 grid-rows-6  h-[700px] w-[1rem]">
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+      </div>
+      <div className="grid grid-cols-10 grid-rows-6 h-[700px] w-[12rem]">
+        <div className="col-span-9 bg-black  "></div>
+
+        {/* Div 5 */}
+        <div className="row-span-4 row-start-2 bg-black  "> </div>
+
+        {/* Div 6 */}
+        <div className="col-span-5 col-start-3 row-start-2 bg-black  "></div>
+
+        {/* Div 7 */}
+        <div className="col-span-3 col-start-4 row-start-3 bg-black  "></div>
+
+        {/* Div 8 */}
+        <div className="row-span-2 row-start-4 col-start-5 bg-black  "></div>
+
+        {/* Div 9 */}
+        <div className="row-span-4 row-start-2 col-start-9 bg-black  "></div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-6 h-[700px] w-[3rem]">
+        {/* Div6 */}
+        <div className="col-span-3 row-span-2 bg-black"> </div>
+
+        {/* Div7 */}
+        <div className="row-span-3 col-start-2 row-start-3 bg-black"> </div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-6  h-[700px] w-[3rem]">
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+        <div className="col-span-1 row-span-5 bg-black"></div>
+        <div className="col-span-1 row-span-5 bg-white"> </div>
+      </div>
+    </section>
+  )
+}
+
+export default Chooghaa
--- app/background/page.tsx
+++ app/background/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Choogha from './components/Choogha'
 import PianoKeys from './components/piano'
 import StripePattern from './components/strip-patern'
+import Chooghaa from './components/Chooghaa'
 
 type Props = {}
 
 function page({}: Props) {
   return (
     <div>
+      <Chooghaa />
       <Choogha />
       <PianoKeys />
       <div className="bg-white">
